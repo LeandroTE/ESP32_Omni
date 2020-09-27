0ceaa85210945f70df81464c307862664160cf79
LeandroTE <leandrosp.TE@gmail.com>
2020-09-27 16:51:50 -0300

    moved wifi code code to interface labrary and added IP information to display

--- main/interface/wifi.h
+++ main/interface/wifi.h
@@ -19,8 +19,20 @@
 #include <string.h>
 #include <time.h>
 
+#include "esp_system.h"
+#include "esp_wifi.h"
+#include "esp_log.h"
+
 #include "freertos/FreeRTOS.h"
+#include "freertos/event_groups.h"
 #include "freertos/task.h"
+
+#include "lwip/err.h"
+#include "lwip/sys.h"
+
+#include "spiffs_vfs.h"
+#include "tft.h"
+#include "tftspi.h"
 
 /***********************************************************************************************************************
  * COSNTANTS
@@ -51,7 +63,7 @@
  * PUBLIC FUNCTIONS
  **********************************************************************************************************************/
 
-// void wifi_init_sta(void);
+void wifi_init_sta(void);
 
 /***********************************************************************************************************************
  * END OF FILE
