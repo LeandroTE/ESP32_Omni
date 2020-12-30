e36086a35a0ce8eb74bc91d87c77af66722adbd1
Leandro Pereira <leandrospte@gmail.com>
2020-12-30 11:54:16 -0300

    move file system function to system.c and start of development of display function

--- main/main.c
+++ main/main.c
@@ -183,35 +183,6 @@ static void lidar_task(void *arg) {
 /***********************************************************************************************************************
  * LOCALS FUNCTIONS
  **********************************************************************************************************************/
-#if CONFIG_EXAMPLE_WEB_DEPLOY_SF
-esp_err_t init_fs(void) {
-    esp_vfs_spiffs_conf_t conf = {.base_path = CONFIG_EXAMPLE_WEB_MOUNT_POINT,
-                                  .partition_label = NULL,
-                                  .max_files = 5,
-                                  .format_if_mount_failed = false};
-    esp_err_t ret = esp_vfs_spiffs_register(&conf);
-
-    if (ret != ESP_OK) {
-        if (ret == ESP_FAIL) {
-            ESP_LOGE(TAG, "Failed to mount or format filesystem");
-        } else if (ret == ESP_ERR_NOT_FOUND) {
-            ESP_LOGE(TAG, "Failed to find SPIFFS partition");
-        } else {
-            ESP_LOGE(TAG, "Failed to initialize SPIFFS (%s)", esp_err_to_name(ret));
-        }
-        return ESP_FAIL;
-    }
-
-    size_t total = 0, used = 0;
-    ret = esp_spiffs_info(NULL, &total, &used);
-    if (ret != ESP_OK) {
-        ESP_LOGE(TAG, "Failed to get SPIFFS partition information (%s).", esp_err_to_name(ret));
-    } else {
-        ESP_LOGI(TAG, "Partition size: total: %d, used: %d", total, used);
-    }
-    return ESP_OK;
-}
-#endif
 
 void app_main() {
     gpio_config_t io_conf;    // Declare GPIO config structure
--- main/system.c
+++ main/system.c
@@ -16,7 +16,7 @@
 /***********************************************************************************************************************
  * COSNTANTS
  **********************************************************************************************************************/
-
+static const char *TAG_SYS = "SUSTEM";
 /***********************************************************************************************************************
  * MACROS
  **********************************************************************************************************************/
@@ -254,6 +254,44 @@ int sendData(const char *data) {
     const int txBytes = uart_write_bytes(UART_NUM_1, data, len);
     return txBytes;
 }
+/***********************************************************************************************************************
+ * @brief Responsible for the initialization of the File System
+ *
+ * @param
+ *
+ * @return void
+ *
+ * ********************************************************************************************************************/
+esp_err_t init_fs(void) {
+    esp_vfs_spiffs_conf_t conf = {.base_path = CONFIG_EXAMPLE_WEB_MOUNT_POINT,
+                                  .partition_label = NULL,
+                                  .max_files = 5,
+                                  .format_if_mount_failed = false};
+    esp_err_t ret = esp_vfs_spiffs_register(&conf);
+
+    if (ret != ESP_OK) {
+        if (ret == ESP_FAIL) {
+            ESP_LOGE(TAG_SYS, "Failed to mount or format filesystem");
+        } else if (ret == ESP_ERR_NOT_FOUND) {
+            ESP_LOGE(TAG_SYS, "Failed to find SPIFFS partition");
+        } else {
+            ESP_LOGE(TAG_SYS, "Failed to initialize SPIFFS (%s)", esp_err_to_name(ret));
+        }
+        return ESP_FAIL;
+    }
+
+    size_t total = 0, used = 0;
+    ret = esp_spiffs_info(NULL, &total, &used);
+    if (ret != ESP_OK) {
+        ESP_LOGE(TAG_SYS, "Failed to get SPIFFS partition information (%s).", esp_err_to_name(ret));
+    } else {
+        ESP_LOGI(TAG_SYS, "Partition size: total: %d, used: %d", total, used);
+    }
+    return ESP_OK;
+}
+
+
+
 /***********************************************************************************************************************
  * END OF FILE
  **********************************************************************************************************************/
